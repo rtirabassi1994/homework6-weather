364e0d72b64f260b0ba029ce6fd53341c71e9d57
rtirabassi1994 <tirabassi.rachel@gmail.com>
2020-10-09 12:55:22 -0400

    commit3

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-
-
 var apiKey = "d59832120efe0beac1c8d9640880067c";
-// //declare and queries as a variable
-
 
 $("#search-input").on("click", function(){
     var input1 = $("#city-search").val()
@@ -29,13 +25,12 @@ $("#search-input").on("click", function(){
             console.log(response);
             var uvIndex = response.value;
 
-        
-        
             // var forecastBlock =`<h1>${cityName}</h1>`
             var weatherBlock = `<div class="jumbotron jumbotron-fluid">
             <div class="container">
-            <h1>${cityName} (${parseInt(date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear()})</h1> 
-            <h2 class="display-4">${fTemp}\u00B0F</h2>
+            <h1>${cityName}</h1>
+            <h2>${parseInt(date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear()}</h2>
+            <h2 class="display-4">${fTemp}</h2>
             <p>Humidity: ${humidity}\u0025</p>
             <p>Wind Speed: ${windSpeed} MPH</p>
             <p>UV Index: ${uvIndex}</p>
@@ -43,59 +38,85 @@ $("#search-input").on("click", function(){
             </div>`
 
             // $("#history-column").html(forecastBlock);
-            // console.log(weatherBlock)
+            console.log(weatherBlock)
             $("#weather-forecast").html(weatherBlock);
 
         })
+
+        console.log(lat);
+        console.log(long);
+        fiveDayForecast(lat, long);
         
-        //fiveDayForecast(userInput);
     })
 
 });
-//make your api calls as functions
-// function getForecast(userInput){
-    // $.ajax({
-    //     type: "GET",
-    //     url: forecastQuery + userInput + apiKey
-    // }).then(function(response){
-    //     console.log(response);
-    //     console.log(response.name);
 
-    //     var forecastBlock =`<h1>${response.name}</h1>`
-
-    //     $("#history-column").html(forecastBlock);
-    //     //fiveDayForecast(userInput);
-    //     //uvIndex(response.coord.lat, response.coord.lon);
-    // })
-// }
-
-function fiveDayForecast(userInput){
-    // var fiveDayQuery = "api.openweathermap.org/data/2.5/forecast?q=" + {city name}+ "&appid" + apiKey;
-    var fiveDayQuery = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&appid=" + apiKey;
+function fiveDayForecast(lat, long){
+    var fiveDayForecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+long+"&appid="+apiKey;
     $.ajax({
-        type: "GET",
-        url: fiveDayQuery
+        url: fiveDayForecastUrl,
+        method: "GET"
     }).then(function(response){
         console.log(response);
 
-})}
+        
+        var date = new Date();
+
+        
+        var fiveDayForecastTitle = $("<h2>5-Day Forecast:</h2>");
+        
+
+        $("#outterContainer").append(fiveDayForecastTitle);
+        var cardRow = $("<div>");
+        cardRow.addClass("row");
+
+        for(var i = 0; i < 5; i++) {
+            var futureTemp = convertTemp(response.daily[i+1].temp.day);
+            var futureHumidity = response.daily[i+1].humidity;
+
+            var nextDay = date.getDate() + (i+1);
+            var futureDate = new Date();
+            futureDate.setDate(nextDay);
+
+            var card = $("<div>");
+            card.addClass("card");
+            card.css("width","20%");
+
+            var cardBody = $("<div>");
+            cardBody.addClass("card-body");
+            cardBody.text(parseInt(futureDate.getMonth()+1)+"/"+futureDate.getDate()+"/"+futureDate.getFullYear());
+            cardBody.append("<br>");
+            cardBody.append("Temp: "+futureTemp);
+            cardBody.append("<br>");
+            cardBody.append("Humidity: "+futureHumidity +"%");
+
+            card.append(cardBody);
+
+            $(cardRow).append(card);
+        }
+        $("#outterContainer").append(cardRow);
+
+    })
+}
+
+// function uvIndex(lat, lon){
+//     uvQuery = "http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}"
 
 
-$(document).ready(function(){
+// }
 
-    // $("#search-input").on("click", getForecast(input1));
-    //when the user clicks search, take input value and store to variable
-    //clear the input box
-    //call your getForecast function with the user input value
-    // getForecast(input1);
-    //make a button/list item for the history search
+// $(document).ready(function(){
 
-})
+//     // $("#search-input").on("click", getForecast(input1));
+//     //when the user clicks search, take input value and store to variable
+//     //clear the input box
+//     //call your getForecast function with the user input value
+//     // getForecast(input1);
+//     //make a button/list item for the history search
+
+// })
 
 function convertTemp(kelvin){
     var cel = kelvin - 273.15;
-    return Math.round(cel * 9/5 + 32)
+    return Math.round(cel * 9/5 + 32)+"\u00B0F"
 }
-
-
-// will probably be able to delete
